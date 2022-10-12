15b7c49b4bf87220378cca9375742d3bdbaac134
Syswea <873550310@qq.com>
2022-10-12 16:40:02 +0800

    master

---
+++ book.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+void Solve () {
+    return ;
+}
+signed main () {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T; cin >> T;
+    while (T -- )
+        Solve ();
+    return 0;
+}
