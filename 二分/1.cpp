103600aa01cf45718df90766610717efce6f9c72
Syswea <873550310@qq.com>
2022-10-31 10:28:37 +0800

    master

---
+++ 二分/1.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+signed main () {
+    int N, K;
+    cin >> N >> K;
+    int S[N + 1] = {}, A[N + 1] = {};
+    for (int i = 1; i <= N; i ++ ) cin >> A[i];
+    for (int i = 1; i <= N; i ++ ) S[i] = S[i - 1] + A[i];
+    int Ans = 0;
+    for (int i = 1; i <= N; i ++ )
+        Ans = max (Ans, S[i] - S[max (i - K - 1, 0ll)]);
+    cout << Ans << '\n' ;
+    return 0;
+}
